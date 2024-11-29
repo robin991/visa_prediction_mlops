0e44d75d345da5990b550d71c27467d8d07b5078
robin991 <47486500+robin991@users.noreply.github.com>
2024-11-30 02:14:10 +0530

    created requirements

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name = "us_visa",
+      version = "0.0.0",
+      author ="robin",
+      author_email="robin__@gmail.com",
+      packages = find_packages())
