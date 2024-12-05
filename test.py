2260cf5cfa402b17dc4adcf6b5f500f30c178fbd
robin991 <47486500+robin991@users.noreply.github.com>
2024-12-06 01:13:53 +0530

    creating first data ingestion pipeline: read from mongo and split

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 from us_visa.logger import logging
 from  us_visa.exception import USVisaException
 logging.info("check this")
-
+import os
 try: 
-    1/0
+    dotenv_path = os.path.join(os.path.dirname(__file__), "us_visa\environment_var", ".env")
+    print(dotenv_path)
 
 except USVisaException as e:
     logging.error(e)
---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,43 @@
+import os
+from datetime import datetime
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), "..", "environment_var", ".env")
+load_dotenv(dotenv_path = dotenv_path)
+
+DATABASE_NAME = "US_VISA_DB"
+
+COLLECTION_NAME = "visa_data"
+
+MONGODB_URL_KEY = os.getenv("MONGODB_URL")
+
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+FILE_NAME: str = "usvisa.csv"
+MODEL_FILE_NAME = "model.pkl"
+
+
+TARGET_COLUMN = "case_status"
+CURRENT_YEAR = datetime.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+
+
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
